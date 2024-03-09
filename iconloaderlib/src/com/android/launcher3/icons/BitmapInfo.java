2276fe25eccfe6beaebdb488d17d4202aada5ada
Tommy Webb <tommy@calyxinstitute.org>
2024-03-09 11:57:44 -0500

    fixup! Multiple work profile badge colors for Launcher3
    Fix for Android 14 QPR2 by always considering a provided UserHandle.
    
    * Don't remove FLAG_WORK and getUserFlagOpLocked.
    * If somehow a user is not associated with a badge icon (which shouldn't
      happen), the baked-in icons are used as fallback, and they're now dark
      and gray so that they don't appear to represent a particular profile.
    
    Test: Manual: Open a work profile app. Open the Overview (Recents). The
    app should have a work profile badge that matches the profile.
    
    Issue: calyxos#2182
    Change-Id: I9b9f01a8e483ee3de1c11a82e8b569537308596d

--- iconloaderlib/src/com/android/launcher3/icons/BitmapInfo.java
+++ iconloaderlib/src/com/android/launcher3/icons/BitmapInfo.java
@@ -30,10 +30,12 @@ import com.android.launcher3.util.FlagOp;
 
 public class BitmapInfo {
 
+    static final int FLAG_WORK = 1 << 0;
     static final int FLAG_INSTANT = 1 << 1;
     static final int FLAG_CLONE = 1 << 2;
     static final int FLAG_PRIVATE = 1 << 3;
     @IntDef(flag = true, value = {
+            FLAG_WORK,
             FLAG_INSTANT,
             FLAG_CLONE,
             FLAG_PRIVATE
@@ -198,6 +200,11 @@ public class BitmapInfo {
     public Drawable getBadgeDrawable(Context context, boolean isThemed) {
         if (badgeInfo != null) {
             return badgeInfo.newIcon(context, isThemed ? FLAG_THEMED : 0);
+        } else if (mUserBadge != null) {
+            // We use a copy of the badge, or changes will affect everywhere it is used;
+            // e.g., shortcuts/widget user badges are very small, and these could affect
+            // regular launcher icons, and the other way around.
+            return mUserBadge.getConstantState().newDrawable().mutate();
         } else if ((flags & FLAG_INSTANT) != 0) {
             return context.getDrawable(isThemed
                     ? R.drawable.ic_instant_app_badge_themed
